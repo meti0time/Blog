0364977cf33674fe487e42b18700ad46b6c49c07
Meti <mehdi.ka78@yahoo.com>
2021-08-29 19:52:33 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="مهدی کاظم پور بازیکن فوتسال و برنامه نویس ">
+  <title>مهدی کاظم پور</title>
+  <link rel="icon" href="dist/img/meti.png"> 
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- bootstrap rtl -->
+  <link rel="stylesheet" href="dist/css/bootstrap-rtl.min.css">
+  <!-- template rtl version -->
+  <link rel="stylesheet" href="dist/css/custom-style.css">
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12 mb-5">
+          <div class="card-body">
+            <div class="row">   
+            <div class="col-md-3"></div>
+              <div class="col-md-6">
+                <div class="card card-widget widget-user">
+                  <div class="widget-user-header text-white" style="background: url('dist/img/background.png') center center;">
+                    <h3 class="widget-user-username">مهدی کاظم پور</h3>
+                  </div>
+                  <div class="widget-user-image">
+                    <img class="img-circle" src="dist/img/user.jpg" alt="User Avatar">
+                  </div>
+                  <div class="card-footer">
+                    <div class="row">
+                      <div class="col-sm-4 border-left">
+                        <div class="description-block">
+                          <h5 class="description-header">575</h5>
+                          <span>Following</span>
+                        </div>
+                        <!-- /.description-block -->
+                      </div>
+                      <!-- /.col -->
+                      <div class="col-sm-4 border-left">
+                        <div class="description-block">
+                          <h5 class="description-header">707</h5>
+                          <span>Followers</span>
+                        </div>
+                        <!-- /.description-block -->
+                      </div>
+                      <!-- /.col -->
+                      <div class="col-sm-4">
+                        <div class="description-block">
+                          <h5 class="description-header">0</h5>
+                          <span>Posts</span>
+                        </div>
+                        <!-- /.description-block -->
+                      </div>
+                      <!-- /.col -->
+                    </div> <br>    
+                    <!-- /.row -->
+                    <div class='row justify-content-center'>
+                        <a href="http://instagram.com/meti7pv">
+                            <i style= "font-size:30px;color:red" class="fa fa-instagram"></i>
+                        </a>&nbsp;&nbsp;&nbsp;
+                        <a href="http://t.me/futsal7">
+                            <i style= "font-size:30px;color:#00CED1" class="fa fa-telegram"></i>
+                        </a>&nbsp;&nbsp;&nbsp;
+                        <a href="http://github.com/meti0time">
+                            <i style= "font-size:30px;color:black" class="fa fa-github"></i>
+                        </a>&nbsp;&nbsp;&nbsp;
+                        <a href="mailto:meti0time@gmail.com">
+                            <i style= "font-size:30px;color:#2F4F4F" class="fa fa-envelope"></i>
+                        </a>
+                     </div>
+                  </div>
+                </div>
+                  <div class="callout callout-danger">
+                    <p>
+                        <i class="fa fa-male"></i> مهدی کاظم پور متولد و ساکن شهر تبریز
+                        <br>
+                        <i style= "color:red" class="fa fa-book"></i> بازیکن فوتسال و برنامه نویس و توسعه دهنده وبسایت و ربات
+                        <br>
+                        <i class="fa fa-graduation-cap"></i> دانشجوی رشته تربیت بدنی در دانشگاه تبریز 
+                        <br>
+                        <i style= "color:blue" class="fa fa-futbol-o"></i> بازیکن تیم های فوتسال مس سونگون
+                        <br>
+                        <i style= "color:green"  class="fa fa-user-secret"></i> مدیر تیم برنامه نویسی توربو تیم 
+                        <br>
+                        <i style= "color:orange" class="fa fa-user"></i> مدیر استارتاپ توربو کد
+                    </p>
+                     <hr>
+                    <strong><i class="fa fa-pencil mr-1"></i> مهارت‌ها</strong>
+                    <p class="text-muted">
+                      <span class="badge badge-success">FooTBall</span>
+                      <span class="badge badge-danger">Php</span>
+                      <span class="badge badge-info">Javascript</span>
+                      <span class="badge badge-warning">Html</span>
+                      <span class="badge badge-primary">Css</span>
+                      <span class="badge badge-secondary">Photoshop</span>
+                      <span class="badge badge-danger">Game</span>
+                    </p>
+                  </div>
+              </div>
+            </div> 
+          </div>
+		</div>
+    </div>
+</div>
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
